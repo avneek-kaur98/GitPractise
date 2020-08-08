64375da4389bcd0257e399dfc504da122f9d72a4
avneek-kaur98 <68783669+avneek-kaur98@users.noreply.github.com>
2020-08-08 21:11:04 +0530

    Initial Commit

---
+++ GitPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractise
+//
+//  Created by Zomato on 08/08/20.
+//  Copyright © 2020 Zomato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
